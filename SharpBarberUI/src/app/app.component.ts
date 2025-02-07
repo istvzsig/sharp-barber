2540bbb5db21423dd60f4481f071eeb503cbdaed
istvzsig <istvanzsigmond@aol.com>
2025-02-07 12:37:14 +0100

    update ui

--- SharpBarberUI/src/app/app.component.ts
+++ SharpBarberUI/src/app/app.component.ts
@@ -1,12 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'SharpBarberUI';
+  barbers: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getBarbers();
+  }
+
+  getBarbers(): void {
+    this.http.get<any[]>('http://localhost:5193/api/barbers').subscribe(
+      (data) => {
+        this.barbers = data;
+        console.log(this.barbers);
+      },
+      (error) => {
+        console.error('Error fetching barbers', error);
+      }
+    );
+  }
 }
