2cf05d139e011be7a68bb134666efa0e31b11049
istvzsig <istvanzsigmond@aol.com>
2025-02-09 11:16:25 +0100

    remove unused imports from app component

--- SharpBarberUI/src/app/app.component.ts
+++ SharpBarberUI/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from "./header/header.component";
-import { BarbersService } from './service/barber.service';
 
 @Component({
   selector: 'app-root',
