43faa5849fe61f894ac82db1b29bf6580c0ec4c4
istvzsig <istvanzsigmond@aol.com>
2025-02-09 15:26:21 +0100

    fix typo

--- SharpBarberUI/src/app/app.routes.ts
+++ SharpBarberUI/src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
-import { BookinsComponent } from './pages/bookins/bookins.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
-    { path: "bookings", component: BookinsComponent },
+    { path: "bookings", component: BookingsComponent },
     { path: '**', component: NotFoundComponent },
 ];
 
