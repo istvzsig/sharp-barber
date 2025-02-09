2133fbc4570f9a9ceeafcab6927ccb075559a648
istvzsig <istvanzsigmond@aol.com>
2025-02-09 14:58:47 +0100

    add bookings route

--- SharpBarberUI/src/app/app.routes.ts
+++ SharpBarberUI/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BookinsComponent } from './pages/bookins/bookins.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
-    { path: "bookings", component: HomeComponent },
+    { path: "bookings", component: BookinsComponent },
     { path: '**', component: NotFoundComponent },
 ];
 
