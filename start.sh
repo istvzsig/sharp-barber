f6a12d50135d34f380844e7784d3b9eb86301aaa
istvzsig <istvanzsigmond@aol.com>
2025-02-09 18:30:26 +0100

    update start script

--- start.sh
+++ start.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-set -e
-
-echo "Starting Backend (SharpBarberAPI)..."
 cd SharpBarberAPI
-dotnet build SharpBarberAPI.csproj
-dotnet run --project SharpBarberAPI.csproj &
+
+dotnet restore
+dotnet ef migrations remove
+
+rm sharpbarber.db
+
+dotnet ef migrations add InitialCreate
+dotnet ef database update
+
+dotnet build
+dotnet run &
 
 sleep 5
 
-echo "Initializing database..."
-dotnet ef database update --project SharpBarberAPI.csproj
-
 cd ..
-
-echo "Starting Angular Frontend (SharpBarberUI)..."
 cd SharpBarberUI
-ng serve --open
-
-wait
+ng serve
