a9a941edd11b5fed82e7dd1ab11daf684bc2ac0b
istvzsig <istvanzsigmond@aol.com>
2025-02-09 17:55:29 +0100

    add db operations to start script

--- start.sh
+++ start.sh
@@ -1,10 +1,21 @@
-#!bin/bash
+#!/bin/bash
 
+set -e
+
+echo "Starting Backend (SharpBarberAPI)..."
 cd SharpBarberAPI
-dotnet build
-dotnet run &
+dotnet build SharpBarberAPI.csproj
+dotnet run --project SharpBarberAPI.csproj &
+
+sleep 5
+
+echo "Initializing database..."
+dotnet ef database update --project SharpBarberAPI.csproj
 
 cd ..
 
+echo "Starting Angular Frontend (SharpBarberUI)..."
 cd SharpBarberUI
-ng serve
+ng serve --open
+
+wait
