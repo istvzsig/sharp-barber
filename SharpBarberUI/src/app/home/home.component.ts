47dc3e42a0a85fad07e882afce375ab37a8b76b8
istvzsig <istvanzsigmond@aol.com>
2025-02-09 11:32:02 +0100

    update home layout and style

--- SharpBarberUI/src/app/home/home.component.ts
+++ SharpBarberUI/src/app/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { VideoBackgroundComponent } from "../video-background/video-background.component";
+import { ScissorsComponent } from "../scissors/scissors.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [VideoBackgroundComponent, ScissorsComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
