0492c6afb1656f7878ca5cb8edeb01bbde5f11df
istvzsig <istvanzsigmond@aol.com>
2025-02-09 11:20:42 +0100

    implement video bakground component

---
+++ SharpBarberUI/src/app/video-background/video-background.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-background',
+  imports: [],
+  templateUrl: './video-background.component.html',
+  styleUrl: './video-background.component.scss'
+})
+export class VideoBackgroundComponent {
+
+}
