d3c9fcac875457ea9eb29f934dec1c97aa39aaf2
istvzsig <istvanzsigmond@aol.com>
2025-02-09 17:28:26 +0100

    update api call in barber angular service

--- SharpBarberUI/src/app/services/barber.service.ts
+++ SharpBarberUI/src/app/services/barber.service.ts
@@ -1,26 +1,22 @@
-// barbers.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, of } from 'rxjs';
+import { Barber } from '../models/barber.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BarberService {
+  private apiUrl = 'http://localhost:5193/api/barbers';
 
   constructor(private http: HttpClient) { }
 
-  getBarbers() {
-    // TODO: Add api call /barbers
-    return [
-      {
-        id: 1,
-        name: "test-barber-1",
-      },
-      {
-        id: 2,
-        name: "test-barber-2",
-      },
-    ]
+  getBarbers(): Observable<Barber[]> {
+    return this.http.get<Barber[]>(this.apiUrl).pipe(
+      catchError(error => {
+        console.error('Error fetching barbers:', error);
+        return of([]);
+      })
+    );
   }
 }
