9bf14ad193db1c9965ad4f7876a18c2e7c57ea07
istvzsig <istvanzsigmond@aol.com>
2025-02-09 14:58:12 +0100

    introduce bookings page

---
+++ SharpBarberUI/src/app/pages/bookins/bookins.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../header/header.component";
+
+@Component({
+  selector: 'app-bookins',
+  imports: [HeaderComponent],
+  templateUrl: './bookins.component.html',
+  styleUrl: './bookins.component.scss'
+})
+export class BookinsComponent {
+
+}
