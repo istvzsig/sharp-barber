f5adadb651dacb8cc6d1aee8afd7a3bacf30f2a7
istvzsig <istvanzsigmond@aol.com>
2025-02-09 14:57:00 +0100

    refactor

--- SharpBarberUI/src/app/app.component.ts
+++ SharpBarberUI/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'SharpBarberUI';
--- SharpBarberUI/src/app/pages/home/home.component.ts
+++ SharpBarberUI/src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { VideoBackgroundComponent } from "../video-background/video-background.component";
-import { ScissorsComponent } from "../scissors/scissors.component";
-import { FooterComponent } from "../footer/footer.component";
+import { VideoBackgroundComponent } from '../../video-background/video-background.component';
+import { ScissorsComponent } from '../../scissors/scissors.component';
+import { FooterComponent } from '../../footer/footer.component';
 
 @Component({
   selector: 'app-home',
