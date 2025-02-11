bbf46fe4270899c7d48200e3f20baeec4838b124
istvzsig <istvanzsigmond@aol.com>
2025-02-11 23:08:27 +0100

    add test to booking form

--- SharpBarberUI/src/app/form/bookingform.component.spec.ts
+++ SharpBarberUI/src/app/form/bookingform.component.spec.ts
@@ -1,22 +1,48 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { BookingFormComponent } from "./bookingform.component";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BookingFormComponent } from './bookingform.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
-describe('BookingformComponent', () => {
+describe('BookingFormComponent', () => {
   let component: BookingFormComponent;
   let fixture: ComponentFixture<BookingFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BookingFormComponent]
-    })
-      .compileComponents();
+      imports: [ReactiveFormsModule, BookingFormComponent],
+      declarations: []
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BookingFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be invalid when fields are empty', () => {
+    expect(component.bookingForm.valid).toBeFalsy();
+  });
+
+  it('should be valid when required fields are filled', () => {
+    component.bookingForm.controls['username'].setValue('JohnDoe');
+    component.bookingForm.controls['appointmentTime'].setValue('2025-02-12T10:00');
+    component.bookingForm.controls['selectedBarber'].setValue(1);
+
+    expect(component.bookingForm.valid).toBeTruthy();
+  });
+
+  it('should call onSubmit when form is valid', () => {
+    spyOn(component, 'onSubmit');
+
+    component.bookingForm.controls['username'].setValue('JohnDoe');
+    component.bookingForm.controls['appointmentTime'].setValue('2025-02-12T10:00');
+    component.bookingForm.controls['selectedBarber'].setValue(1);
+
+    fixture.detectChanges();
+    component.onSubmit();
+
+    expect(component.onSubmit).toHaveBeenCalled();
   });
 });
