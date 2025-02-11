67dc13d51f5332c91f2817d353b74398bd7755b5
istvzsig <istvanzsigmond@aol.com>
2025-02-11 23:08:51 +0100

    fix barber service test

--- SharpBarberUI/src/app/services/barber.service.spec.ts
+++ SharpBarberUI/src/app/services/barber.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BarberService } from './barber.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('BarberService', () => {
   let service: BarberService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler]
+    });
     service = TestBed.inject(BarberService);
   });
 
