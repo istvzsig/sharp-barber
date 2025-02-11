2e560bcd2f267edf5bfadde933bced3c140d6a59
istvzsig <istvanzsigmond@aol.com>
2025-02-11 22:18:06 +0100

    update form to reactive angular form

--- SharpBarberUI/src/app/form/bookingform.component.ts
+++ SharpBarberUI/src/app/form/bookingform.component.ts
@@ -1,30 +1,39 @@
 import { Component, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BarberSelectorComponent } from '../components/barber-selector/barber-selector.component';
 import { Barber } from '../models/barber.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bookingform',
-  imports: [FormsModule, BarberSelectorComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, BarberSelectorComponent],
   templateUrl: './bookingform.component.html',
   styleUrl: './bookingform.component.scss'
 })
 export class BookingFormComponent {
-  username: string = '';
-  appointmentTime: string = '';
+  bookingForm: FormGroup;
   selectedBarber: Barber | null = null;
 
   @Input() barbers: Barber[] = [];
 
+  constructor(private fb: FormBuilder) {
+    this.bookingForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(13)]],
+      appointmentTime: ['', [Validators.required]],
+      selectedBarber: [null, [Validators.required]]
+    })
+  }
+
   onBarberSelected(barber: Barber) {
     this.selectedBarber = barber;
+    this.bookingForm.patchValue({ selectedBarber: barber });
   }
 
   onSubmit() {
-    console.log('Booking:', {
-      username: this.username,
-      appointmentTime: this.appointmentTime,
-      selectedBarber: this.selectedBarber,
-    });
+    if (this.bookingForm.valid) {
+      console.log('Booking:', this.bookingForm.value);
+    } else {
+      console.error('Form is invalid');
+    }
   }
 }
